75f805e827e1bfc12a3818cf08502555ecd6de06
Mircea Amarascu <mircea@ag-projects.com>
2007-02-14 17:23:31 +0000

    Also rename them in test/crypto.py

--- test/crypto.py
+++ test/crypto.py
@@ -9,10 +9,10 @@ from gnutls.crypto import *
 
 certs_path = os.path.join(script_path, 'certs')
 
-cert = X509Cert(open(certs_path + '/valid.crt').read())
-key = PrivateKey(open(certs_path + '/valid.key').read())
+cert = X509Certificate(open(certs_path + '/valid.crt').read())
+key = X509PrivateKey(open(certs_path + '/valid.key').read())
 
-ca = X509Cert(open(certs_path + '/ca.pem').read())
+ca = X509Certificate(open(certs_path + '/ca.pem').read())
 crl = X509CRL(open(certs_path + '/crl.pem').read())
 
 print 'Cert subject: ', cert.subject
