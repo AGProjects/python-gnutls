f8f060085f3ad001a6731e0668adca9c380e9e10
Dan Pascu <dan@ag-projects.com>
2007-03-19 11:30:06 +0000

    Do not verify credentials in the speed test servers

--- test/ts-gnutls.py
+++ test/ts-gnutls.py
@@ -34,13 +34,17 @@ class EchoFactory(Factory):
     protocol = EchoProtocol
     noisy = False
 
+class DontVerifyCredentials(Credentials):
+    def verify_callback(self, peer_cert, preverify_status=None):
+        pass
+
 certs_path = os.path.join(script_path, 'certs')
 
 cert = X509Certificate(open(certs_path + '/valid.crt').read())
 key = X509PrivateKey(open(certs_path + '/valid.key').read())
 ca = X509Certificate(open(certs_path + '/ca.pem').read())
 crl = X509CRL(open(certs_path + '/crl.pem').read())
-cred = Credentials(cert, key, [ca], [crl])
+cred = DontVerifyCredentials(cert, key, [ca], [crl])
 
 reactor.listenTLS(10000, EchoFactory(), cred)
 reactor.run()
