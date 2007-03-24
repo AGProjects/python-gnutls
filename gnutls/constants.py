206a26c07c4d800f65d994591a765ec01b4a5a5e
Dan Pascu <dan@ag-projects.com>
2007-03-24 00:49:14 +0000

    Removed unused constants

--- gnutls/constants.py
+++ gnutls/constants.py
@@ -7,9 +7,6 @@ __all__ = [
     ## Certificate formats
     'X509_FMT_DER', 'X509_FMT_PEM',
 
-    ## Certificate verification status
-    'CERT_INVALID', 'CERT_REVOKED', 'CERT_SIGNER_NOT_FOUND', 'CERT_SIGNER_NOT_CA', 'CERT_INSECURE_ALGORITHM',
-    
     ## GNUTLS session protocols
     'PROTO_TLS1_1', 'PROTO_TLS1_0', 'PROTO_SSL3',
     
