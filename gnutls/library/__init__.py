f9538f4d7a95f4533ded965eb405565ff35202ac
Dan Pascu <dan@ag-projects.com>
2007-04-02 15:34:40 +0000

    Check GNUTLS library version at runtime

--- gnutls/library/__init__.py
+++ gnutls/library/__init__.py
@@ -6,3 +6,11 @@ from gnutls.library import constants
 from gnutls.library import types
 from gnutls.library import functions
 
+__need_version__ = '1.4.4'
+
+if functions.gnutls_check_version(__need_version__) is None:
+    version = functions.gnutls_check_version(None)
+    raise RuntimeError("Found GNUTLS library version %s, but at least version %s is required" % (version, __need_version__))
+if functions.gnutls_extra_check_version(__need_version__) is None:
+    version = functions.gnutls_extra_check_version(None)
+    raise RuntimeError("Found GNUTLS extra library version %s, but at least version %s is required" % (version, __need_version__))
