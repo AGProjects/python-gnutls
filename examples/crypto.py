aae38597aa22e31f50216f5919694c58e3c4e369
Dan Pascu <dan@ag-projects.com>
2007-04-03 16:05:43 +0000

    Better crypto example script

--- examples/crypto.py
+++ examples/crypto.py
@@ -11,7 +11,6 @@ script_path = os.path.realpath(os.path.dirname(sys.argv[0]))
 certs_path = os.path.join(script_path, 'certs')
 
 cert = X509Certificate(open(certs_path + '/valid.crt').read())
-revoked_cert = X509Certificate(open(certs_path + '/revoked.crt').read())
 key = X509PrivateKey(open(certs_path + '/valid.key').read())
 ca = X509Certificate(open(certs_path + '/ca.pem').read())
 crl = X509CRL(open(certs_path + '/crl.pem').read())
@@ -19,6 +18,13 @@ crl = X509CRL(open(certs_path + '/crl.pem').read())
 print 'Cert subject:   ', cert.subject
 print 'Cert issuer:    ', cert.issuer
 print 'Cert serial:    ', cert.serial_number
-print 'Check hostname: ', cert.has_hostname('test.example.com')
-print 'Certificate was revoked: ', crl.is_revoked(revoked_cert)
+print 'Certificate was revoked: ', crl.is_revoked(cert)
+print ""
 
+cert = X509Certificate(open(certs_path + '/revoked.crt').read())
+
+print 'Cert subject:   ', cert.subject
+print 'Cert issuer:    ', cert.issuer
+print 'Cert serial:    ', cert.serial_number
+print 'Certificate was revoked: ', crl.is_revoked(cert)
+print ""
