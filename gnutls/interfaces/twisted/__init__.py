033ceef026b4a8a273062d18ba622a21f313333f
Dan Pascu <dan@ag-projects.com>
2007-04-18 14:20:05 +0000

    Set the transport on protocol after the TCP connection is made for TLSPort

--- gnutls/interfaces/twisted/__init__.py
+++ gnutls/interfaces/twisted/__init__.py
@@ -319,6 +319,7 @@ class TLSPort(tcp.Port):
 
     def _preMakeConnection(self, transport):
         transport.protocol.makeConnection = lambda *args: None
+        transport.protocol.transport = transport ## because we may call connectionLost before connectionMade
         transport.startTLS()
         return tcp.Port._preMakeConnection(self, transport)
 
