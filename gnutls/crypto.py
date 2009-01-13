81975e0bea8f37a03789a432864897a074f75e50
Dan Pascu <dan@ag-projects.com>
2009-01-13 11:35:32 +0000

    Fixed DeprecationWarning when running with python2.6 or newer

--- gnutls/crypto.py
+++ gnutls/crypto.py
@@ -42,7 +42,7 @@ class X509Name(str):
     __metaclass__ = X509NameMeta
 
     def __init__(self, dname):
-        str.__init__(self, dname)
+        str.__init__(self)
         pairs = [x.replace('\,', ',') for x in re.split(r'(?<!\\),\s*', dname)]
         for pair in pairs:
             try:
