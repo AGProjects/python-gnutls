d9624659114143d3ec0054ce5b32289a6a598d9d
Dan Pascu <dan@ag-projects.com>
2010-02-01 15:20:57 +0000

    Improved error handling

--- gnutls/library/errors.py
+++ gnutls/library/errors.py
@@ -22,55 +22,63 @@ class ErrorMessage(str):
 
 # Check functions which return an integer status code (negative codes being errors)
 #
-def _check_status(retcode, function, args):
-    if retcode >= 0:
-        return retcode
-    elif retcode == -1:
-        from gnutls.library import functions
-        if function in (functions.gnutls_certificate_activation_time_peers,
-                        functions.gnutls_x509_crt_get_activation_time,
-                        functions.gnutls_openpgp_key_get_creation_time):
-            raise GNUTLSError("cannot retrieve activation time")
-        elif function in (functions.gnutls_certificate_expiration_time_peers,
-                          functions.gnutls_x509_crt_get_expiration_time,
-                          functions.gnutls_openpgp_key_get_expiration_time):
-            raise GNUTLSError("cannot retrieve expiration time")
-        elif function in (functions.gnutls_x509_crl_get_this_update,
-                          functions.gnutls_x509_crl_get_next_update):
-            raise GNUTLSError("cannot retrieve CRL update time")
+class ErrorHandler(object):
+    alert_map = {
+        GNUTLS_A_BAD_CERTIFICATE       : CertificateError("peer rejected our certificate as invalid"),
+        GNUTLS_A_UNKNOWN_CA            : CertificateAuthorityError("peer does not trust our certificate authority"),
+        GNUTLS_A_INSUFFICIENT_SECURITY : CertificateSecurityError("peer rejected us on insufficient security"),
+        GNUTLS_A_CERTIFICATE_EXPIRED   : CertificateExpiredError("peer rejected our certificate as expired"),
+        GNUTLS_A_CERTIFICATE_REVOKED   : CertificateRevokedError("peer rejected our certificate as revoked")
+    }
+
+    function_map = dict(
+        gnutls_certificate_activation_time_peers          = "cannot get certificate activation time",
+        gnutls_certificate_expiration_time_peers          = "cannot get certificate expiration time",
+        gnutls_x509_crl_get_next_update                   = "cannot get CRL's next update time",
+        gnutls_x509_crl_get_this_update                   = "cannot get CRL's issue time",
+        gnutls_x509_crt_get_activation_time               = "cannot get X509 certificate activation time",
+        gnutls_x509_crt_get_expiration_time               = "cannot get X509 certificate expiration time",
+        gnutls_openpgp_crt_get_creation_time              = "cannot get OpenPGP key creation time",
+        gnutls_openpgp_crt_get_expiration_time            = "cannot get OpenPGP key expiration time",
+        gnutls_openpgp_crt_get_subkey_creation_time       = "cannot get OpenPGP subkey creation time",
+        gnutls_openpgp_crt_get_subkey_expiration_time     = "cannot get OpenPGP subkey expiration time",
+        gnutls_openpgp_privkey_get_subkey_creation_time   = "cannot get OpenPGP subkey creation time",
+        gnutls_openpgp_privkey_get_subkey_expiration_time = "cannot get OpenPGP subkey expiration time"
+    )
+
+    @classmethod
+    def check_status(cls, retcode, function, args):
+        if retcode >= 0:
+            return retcode
+        elif retcode == -1:
+            raise GNUTLSError(cls.function_map.get(function.__name__) or ErrorMessage(retcode))
+        elif retcode == GNUTLS_E_AGAIN:
+            raise OperationWouldBlock(gnutls_strerror(retcode))
+        elif retcode == GNUTLS_E_INTERRUPTED:
+            raise OperationInterrupted(gnutls_strerror(retcode))
+        elif retcode in (GNUTLS_E_MEMORY_ERROR, GNUTLS_E_SHORT_MEMORY_BUFFER):
+            raise MemoryError(ErrorMessage(retcode))
+        elif retcode == GNUTLS_E_NO_CERTIFICATE_FOUND:
+            raise CertificateSecurityError(gnutls_strerror(retcode))
+        elif retcode == GNUTLS_E_FATAL_ALERT_RECEIVED:
+            exception = cls.alert_map.get(gnutls_alert_get(args[0]))
+            raise exception and exception.__class__(*exception.args) or GNUTLSError(ErrorMessage(retcode))
+        elif retcode == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE:
+            raise RequestedDataNotAvailable(gnutls_strerror(retcode))
         else:
             raise GNUTLSError(ErrorMessage(retcode))
-    elif retcode == GNUTLS_E_AGAIN:
-        raise OperationWouldBlock(gnutls_strerror(retcode))
-    elif retcode == GNUTLS_E_INTERRUPTED:
-        raise OperationInterrupted(gnutls_strerror(retcode))
-    elif retcode in (GNUTLS_E_MEMORY_ERROR, GNUTLS_E_SHORT_MEMORY_BUFFER):
-        raise MemoryError(ErrorMessage(retcode))
-    elif retcode == GNUTLS_E_NO_CERTIFICATE_FOUND:
-        raise CertificateSecurityError(gnutls_strerror(retcode))
-    elif retcode == GNUTLS_E_FATAL_ALERT_RECEIVED:
-        alertdict = {
-            GNUTLS_A_BAD_CERTIFICATE: (CertificateError, "peer rejected our certificate as invalid"),
-            GNUTLS_A_UNKNOWN_CA: (CertificateAuthorityError, "peer does not trust our certificate authority"),
-            GNUTLS_A_INSUFFICIENT_SECURITY: (CertificateSecurityError, "peer rejected us on insufficient security"),
-            GNUTLS_A_CERTIFICATE_EXPIRED: (CertificateExpiredError, "peer rejected our certificate as expired"),
-            GNUTLS_A_CERTIFICATE_REVOKED: (CertificateRevokedError, "peer rejected our certificate as revoked")
-        }
-        alert = gnutls_alert_get(args[0])
-        exception, reason = alertdict.get(alert, (GNUTLSError, ErrorMessage(retcode)))
-        raise exception(reason)
-    elif retcode == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE:
-        raise RequestedDataNotAvailable(gnutls_strerror(retcode))
-    else:
-        raise GNUTLSError(ErrorMessage(retcode))
+
 
 # Attach the error checking function to all functions returning integers
 #
 from gnutls.library import functions
 from ctypes import c_int, c_long
 
+if not set(functions.__all__).issuperset(ErrorHandler.function_map):
+    raise RuntimeError("ErrorHandler.function_map is not synchronized with the function names anymore")
+
 for func in (obj for name, obj in functions.__dict__.iteritems() if name in functions.__all__ and obj.restype in (c_int, c_long)):
-    func.errcheck = _check_status
+    func.errcheck = ErrorHandler.check_status
 
 del c_int, c_long, func, functions
 
