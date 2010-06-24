63e5a81772558d62c72964ef42f16443804aff0a
Dan Pascu <dan@ag-projects.com>
2010-06-24 13:13:23 +0000

    Allow extension to be build with mingw on windows

--- setup.py
+++ setup.py
@@ -1,22 +1,69 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 from gnutls import __version__
 import os
 import re
-import platform
+import sys
+
+
+class BuildExtension(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        for extension in self.extensions:
+            extension.compiler = self.compiler
+
+
+class GNUTLSInit(object, Extension):
+    def __init__(self):
+        Extension.__init__(self, name='gnutls.library._init', sources=['gnutls/library/_init.c'])
+        self.compiler = None
+
+    def _get_include_dirs(self):
+        if sys.platform == 'win32':
+            if self.compiler == 'mingw32':
+                return [r'C:\MinGW\include']
+            else:
+                return [r'C:\Developer\include']
+        else:
+            return []
+
+    def _get_library_dirs(self):
+        if sys.platform == 'win32':
+            if self.compiler == 'mingw32':
+                return [r'C:\MinGW\lib']
+            else:
+                return [r'C:\Developer\lib']
+        else:
+            return []
+
+    def _get_libraries(self):
+        if sys.platform=='win32' and self.compiler in ('msvc', None):
+            return ['pthreadVC2']
+        else:
+            return ['pthread']
+
+    def _set_include_dirs(self, value):
+        pass
+
+    def _set_library_dirs(self, value):
+        pass
+
+    def _set_libraries(self, value):
+        pass
+
+    include_dirs = property(_get_include_dirs, _set_include_dirs)
+    library_dirs = property(_get_library_dirs, _set_library_dirs)
+    libraries = property(_get_libraries, _set_libraries)
+
+    del _get_include_dirs, _set_include_dirs
+    del _get_library_dirs, _set_library_dirs
+    del _get_libraries, _set_libraries
+
 
 def find_packages(toplevel):
     return [directory.replace('/', '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
-
-if platform.system() == 'Windows':
-    include_dirs = [r'C:\Developer\include']
-    library_dirs = [r'C:\Developer\lib']
-    libraries = ['pthreadVC2']
-else:
-    include_dirs = []
-    library_dirs = []
-    libraries = ['pthread']
 
 # Get the title and description from README
 readme = open('README').read()
@@ -41,9 +88,6 @@ setup(name         = "python-gnutls",
         "Topic :: Software Development :: Libraries :: Python Modules"
       ],
       packages     = find_packages('gnutls'),
-      ext_modules  = [Extension(name='gnutls.library._init',
-                          sources=['gnutls/library/_init.c'],
-                          include_dirs=include_dirs,
-                          library_dirs=library_dirs,
-                          libraries=libraries)])
+      ext_modules  = [GNUTLSInit()],
+      cmdclass     = {'build_ext': BuildExtension})
 
