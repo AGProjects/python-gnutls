28a7aed1f0993e453e9713f8ae2d7a607ac95f15
Saul Ibarra <saul@ag-projects.com>
2014-06-25 09:48:16 +0000

    Remove no longer needed workaround

--- gnutls/__init__.py
+++ gnutls/__init__.py
@@ -3,16 +3,3 @@
 
 __version__ = '1.2.5'
 
-
-# Fix threading issue with older ctypes. Prior to ctypes 1.0.3 string_at and
-# wstring_at were not thread safe because they were declared using CFUNCTYPE
-# which releases the GIL, but the functions call python C API functions that
-# need the GIL to be acquired. See http://bugs.python.org/issue3554
-#
-import ctypes
-if hasattr(ctypes, '_string_at') and isinstance(ctypes._string_at, ctypes._CFuncPtr) and not (ctypes._string_at._flags_ & ctypes._FUNCFLAG_PYTHONAPI):
-    ctypes._string_at = ctypes.PYFUNCTYPE(ctypes.py_object, ctypes.c_void_p, ctypes.c_int)(ctypes._string_at_addr)
-    if hasattr(ctypes, '_wstring_at'):
-        ctypes._wstring_at = ctypes.PYFUNCTYPE(ctypes.py_object, ctypes.c_void_p, ctypes.c_int)(ctypes._wstring_at_addr)
-del ctypes
-
