2f73ca72635820f67c0c4bd71c712a13febe7ad4
Dan Pascu <dan@ag-projects.com>
2020-02-14 09:15:11 +0000

    Increased version and updated changelogs

--- gnutls/__info__.py
+++ gnutls/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python-gnutls"
 __summary__ = "Python wrapper for the GnuTLS library"
 __webpage__ = "https://github.com/AGProjects/python-gnutls"
 
-__version__ = "3.1.2"
+__version__ = "3.1.3"
 
 __author__ = "Dan Pascu"
 __email__ = "dan@ag-projects.com"
